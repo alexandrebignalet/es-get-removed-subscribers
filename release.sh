d5eeb86eb51a9b3065e89377d17c5e8e2bcbb6c1
alexandrebignalet <alexandre.bignalet@gmail.com>
2018-12-16 15:23:31 +0100

    added deploy script

--- release.sh
+++ release.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
-
 git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+git remote add deploy ssh://git@$SERVER_IP_ADDRESS:$DEPLOY_DIR
 git push deploy master
 
-# Skip this command if you don't need to execute any additional commands after deploying.
-ssh apps@$IP -p $PORT <<EOF
+ssh git@$SERVER_IP_ADDRESS <<EOF
   cd $DEPLOY_DIR
-  crystal build --release --no-debug index.cr # Change to whatever commands you need!
+  docker-compose build
+  docker-compose up --no-deps -d
 EOF
