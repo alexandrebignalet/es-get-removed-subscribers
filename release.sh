efff5540e3872d4ab1720e0bb7a542cf9ac519ac
alexandrebignalet <alexandre.bignalet@gmail.com>
2018-12-16 18:58:04 +0100

    updated release script

--- release.sh
+++ release.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-ssh git@${SERVER_IP_ADDRESS} <<EOF
-  git clone git@github.com:alexandrebignalet/es-get-removed-subscribers.git
+ssh -T -i ./deploy_key git@${SERVER_IP_ADDRESS} <<EOF
+  echo "Deleting existing project"
+  rm -rf es-get-removed-subscribers/
+
+  git clone https://github.com/alexandrebignalet/es-get-removed-subscribers.git
+
+  echo "Getting in the cloned project"
+  cd es-get-removed-subscribers/
+
+  echo "Building and running the container"
   docker-compose build
   docker-compose up --no-deps -d
 EOF
