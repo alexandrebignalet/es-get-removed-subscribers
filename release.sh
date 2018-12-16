54d07d8c3a598af0540fa2dc7b7437e4976ef99c
alexandrebignalet <alexandre.bignalet@gmail.com>
2018-12-16 15:23:31 +0100

    added deploy script

--- release.sh
+++ release.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
-
-git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
-git push deploy master
-
-# Skip this command if you don't need to execute any additional commands after deploying.
-ssh apps@$IP -p $PORT <<EOF
-  cd $DEPLOY_DIR
-  crystal build --release --no-debug index.cr # Change to whatever commands you need!
+ssh git@${SERVER_IP_ADDRESS} <<EOF
+  git clone git@github.com:alexandrebignalet/es-get-removed-subscribers.git
+  docker-compose build
+  docker-compose up --no-deps -d
 EOF
